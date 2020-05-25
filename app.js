ca3fc6e137c5aae074c25b10ad00e3e05ca83dcc
MHuiG <616001163@qq.com>
2020-05-25 12:34:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ res.header("Access-Control-Allow-Origin", "*");
 res.header("Access-Control-Allow-Headers", "X-Requested-With");
 res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
 res.header("X-Powered-By", ' 3.2.1');
-res.header("Content-Type", "application/json;charset=utf-8");
 next(); // 执行下一个路由
 })
 //查询IP
